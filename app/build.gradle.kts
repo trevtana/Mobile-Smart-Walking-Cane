6fedc1156fb0b5d629f29a407cfe189ca02b4765
threesixty <148873439+trevtana@users.noreply.github.com>
2025-04-16 20:49:31 +0700

    Update UI(for settings and profile page)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     // MQTT
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
 
+    //Circle image
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
